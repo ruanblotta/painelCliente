377fa2675179b021e8f352718fc93c3c45bcd929
Ruan Blotta <ruanblotta@gmail.com>
2024-06-28 18:41:22 -0300

    Implementando os métodos AddCliente, UpdateCliente, DeleteCliente relacionados e com interação com o banco de dados

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace ClienteModels
+namespace ClientePainel.Data
 {
     public class AplicationDbContext : DbContext
     {
         public AplicationDbContext(DbContextOptions<ApplicationBuilder> options) : base(options)
         {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .HasKey(c => c.ID_Cliente);
         }
     }
 }
---
+++ Models/Cliente.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ClientePainel.Data;
+
+namespace ClientePainel.Models
+{
+    public class Cliente
+    {
+        public int ID_Cliente { get; set; }
+        public string? nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+
+        
+    public void AddCliente(ApplicationDbContext context)
+    {
+        context.Clientes.Add(this);
+        context.SaveChanges();
+    }
+
+    public void UpdateCliente(ApplicationDbContext context)
+    {
+        context.Clientes.Update(this);
+        context.SaveChanges();
+    }
+
+    public static void DeleteCliente(int id, ApplicationDbContext context)
+    {
+        var cliente = context.Clientes.Find(id);
+        if (cliente != null)
+        {
+            context.Clientes.Remove(cliente);
+            context.SaveChanges();
+        }
+    }
+    }
+}
