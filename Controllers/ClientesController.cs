27cf33cb2f98b906c05f3995bf9767c1cda8a011
Ruan Blotta <ruanblotta@gmail.com>
2024-06-28 18:59:22 -0300

    Implementado os métodos do Controller sob escopo de readonly

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -19,5 +19,56 @@ namespace ClientePainel.Controllers
             List<ClientesController> clientes = Cliente.GetClientes(_context);
             return View(clientes);
         }
+
+        [HttpPost]
+        public IActionResult Create(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                cliente.AddCliente(_context);
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var cliente = _context.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                cliente.UpdateCliente(_context);
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var cliente = _context.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(in id)
+        {
+            Cliente.DeleteCliente(id, _context);
+            return RedirectToAction("Index");
+        }
+
     }
+
 }
